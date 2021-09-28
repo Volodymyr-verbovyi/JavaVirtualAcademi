9b669c150a0ab8f5d1fe35a9f8f4d331435a397e
voha <zi4.psy@gmail.com>
2021-09-28 10:58:22 +0300

    Lesson 04

---
+++ src/main/java/Zminna.java
@@ -0,0 +1,19 @@
+public class Zminna {
+    public static void main(String[] args) {
+        int zminna1 = 7;
+
+        int x;
+        x = 2;
+
+        int y;
+        y = 34;
+        int z;
+        z = 8 * x + 2 * y;
+        System.out.println(z);
+
+        int zminna_X;
+        zminna_X = 5;
+        zminna_X=10;
+        System.out.println(zminna_X);
+    }
+}
