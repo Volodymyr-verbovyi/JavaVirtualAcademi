55aeaaf2e4fa33edbbf09baab3468bf609c8d5f3
voha <zi4.psy@gmail.com>
2021-09-28 14:28:44 +0300

    Lesson 08

--- src/main/java/Lesson06/VvedennyaVidKoristuvacha.java
+++ src/main/java/Lesson06/VvedennyaVidKoristuvacha.java
@@ -1,5 +1,14 @@
 package Lesson06;
 
+import java.util.Scanner;
+
 public class VvedennyaVidKoristuvacha {
 
+    public static void main(String[] args) {
+        System.out.println("Введить свое рик народження: ");
+        Scanner myScaner = new Scanner(System.in);
+        int vik = 2021 - myScaner.nextInt();
+
+        System.out.println("Привит, ваш вик " + vik);
+    }
 }
